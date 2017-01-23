04d8fb769b4087e48a1549b871cc2f0dea2bd61b
melkotoury <melkotoury@gmail.com>
2017-01-23 20:03:51 +0200

    feat: finish till fun facts

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by melkotoury on 1/23/17.
+ */
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
